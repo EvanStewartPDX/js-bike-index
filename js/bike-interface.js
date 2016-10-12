a4d08438e5198a1d26ca8e2e353ad1ae6e15bf5d
Angie Smith and evan stewart <angie@>
2016-10-12 11:54:56 -0700

    add some variables to handle date searches and timestamps.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,14 +1,24 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
 $(document).ready(function() {
-  var bike = new Bike();
-
   $('#search').submit(function(event) {
-
     event.preventDefault();
+    var bike = new Bike();
+
     var color = "purple";
-    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&colors=' + color + '&manufacturer=giant&proximity=45.521728%2C-122.67326&proximity_square=100').then(function(response) {
-      console.log(response);
+    var stamp = "1476264600";
+    var myDate = new Date("10/03/2016"); // Your timezone!
+    var before = myDate.getTime()/1000.0;
+    // var before = "";
+
+    var myDate1 = new Date("10/01/2016"); // Your timezone!
+    var after = myDate1.getTime()/1000.0;
+    // var after = "";
+    console.log(before, after);
+
+    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=10&proximity=45.521728%2C-122.67326&proximity_square=0&stolen_before='+ before +'&stolen_after='+ after).then(function(response) {
+      // console.log(bikes.title);
     });
+    $('h1').text(response.bikes.title);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -1,8 +1,16 @@
-var apiKey = require('./../.env').apiKey;
-
 Bike = function() {
 
 }
 
+Bike.prototype.getBike(make, color, date_before, date_after, zip) {
+
+  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=100&proximity=45.521728%2C-122.67326&proximity_square=0&stolen_before='+ before +'&stolen_after='+ after).then(function(response) {
+    // console.log(bikes.title);
+    response.bikes.forEach
+    for (var bike in response.bikes) {
+
+    }
+  });
+}
 
 exports.bikeModule = Bike;
