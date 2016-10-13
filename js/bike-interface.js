e5aed2fdda2cdb4ffe3be65b1780c1b3e52a45f5
angie smith and evan stewart <evan@>
2016-10-13 15:14:31 -0700

    give background image, create centered stylings

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -5,6 +5,12 @@ var displayBikes = function(bikes) {
 };
 var displayZips = function(zip, count) {
   $("#showzips").append("<p class='zipclick'>" + zip + "</p>");
+  $('.zipclick').last().click(function() {
+    $('#map').hide();
+    $('#map').delay(500).html('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?q=' +
+		zip +
+		'&key=AIzaSyC-u-zGtcSYGbK84Yxq9a_cY76NyKvHoVs&zoom=12" allowfullscreen></iframe>').fadeIn(500);
+  });
   $("#showcount").append("<p> " + count + " </p>");
 };
 
@@ -17,31 +23,15 @@ $(document).ready(function() {
     var color = $('#color').val();
     var date_before = new Date($("#before").val());
     var date_after = new Date($("#after").val());
-    // var date_before = new Date('10/12/2016');
-    // var date_after = new Date('08/01/2016');
 
-    // var color = '';
-    // var make = '';
-    // var zip = "97210";
     var before = date_before.getTime()/1000.0;
     var after = date_after.getTime()/1000.0;
     var newSearch = new Search(color, make, zip, before, after);
 
     newSearch.getBike(displayBikes, displayZips);
+  });
 
-    $('#showzips, .zipclick').click(function (event) {
-		event.preventDefault();
-    alert("hi");
-		format(qArray);
-		// $('.hid').slideUp();
-		$('#map').delay(500).append('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?q=' +
-		search.zip +
-		'&key=AIzaSyC-u-zGtcSYGbK84Yxq9a_cY76NyKvHoVs&zoom=12" allowfullscreen></iframe>').fadeIn(500);
-	})  // console.log(bikes);
-      // setTimeout(bikeText, 5000);
-      //
-      // function bikeText(){
-      //   $(".showbikes").text(newbike.abikes);
-      // }
+  $('#clear').click(function() {
+    location.reload();
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -11,16 +11,10 @@ function Search(color, make, zip, before, after){
 Search.prototype.getBike = function(displayBikes, displayZips) {
   var that = this;
   $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=100&colors=' + that.color + '&manufacturer=' + that.make + '&proximity=' + that.zip + '&proximity_square=5&stolen_before='+ that.before + '&stolen_after=' + that.after).then(function(response) {
-      // for (var bike of response.bikes) {
-      //   var title = bike.title;
-      //   bikes.push(bike);
-      // }
-      // console.log(that.zip);
-      that.getZips(response.bikes, displayZips);
-      displayBikes(response.bikes.length);
-    });
-    // console.log("abikes after request: " + this.abikes);
-    // return this.abikes;
+    console.log(response.bikes);
+    that.getZips(response.bikes, displayZips);
+    displayBikes(response.bikes.length);
+  });
 };
 
 Search.prototype.getZips = function(bikes, displayZips) {
@@ -31,21 +25,10 @@ Search.prototype.getZips = function(bikes, displayZips) {
     var location = bike.stolen_location;
     var part = location.split(",");
     var stolenzip = part[2];
-    // if(ziparray.indexOf(stolenzip) === -1 && !isNaN(stolenzip)) {
-      ziparray.push(stolenzip);
-    // }
+    ziparray.push(stolenzip);
   }
-  // for( var i = 0; i < ziparray.length; ++i) {
-  //   if(!result[ziparray[i]])
-  //     result[ziparray[i]] = 0;
-  //     ++result[ziparray[i]];
-  //
-  // }
-  // var resultsplit = result.split(",");
-  // var resultpart = resultsplit[0];
-  // console.log(result);
+
   this.countZips(ziparray, displayZips);
-  // displayZips(ziparray);
 };
 
 Search.prototype.countZips = function(ziparray, displayZips)  {
@@ -53,7 +36,7 @@ Search.prototype.countZips = function(ziparray, displayZips)  {
   var count = 0;
   zipCount = [];
   var zips = ziparray.sort();
-  for (var i = 0; i < zips.length; i++) {
+  for (var i = 0; i <= zips.length; i++) {
     if (zips[i] != zip) {
       if (count > 0) {
         displayZips(zip, count);
