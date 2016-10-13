b7d63d763b49369e3b89aa76f74edc6e113b8621
angie smith and evan stewart <angie@>
2016-10-13 09:34:00 -0700

    getzips works

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,9 +1,16 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
+var displayBikes = function(bikes) {
+  $(".showbikes").text(bikes);
+};
+var displayZips = function(stolenzips) {
+  $(".showzips").append("<li>" + stolenzips + "</li>");
+};
+
 $(document).ready(function() {
   $('#search').submit(function(event) {
     event.preventDefault();
-    var newbike = new Bike();
+    // console.log('sup')
 
     // var make = $('#make').val();
     // var zip = $('#zip').val();
@@ -11,20 +18,21 @@ $(document).ready(function() {
     // var date_before = new Date($("#before").val());
     // var date_after = new Date($("#after").val());
     var date_before = new Date('10/12/2016');
-    var date_after = new Date('8/01/2016');
+    var date_after = new Date('08/01/2016');
 
-    var color = 'blue';
+    var color = '';
     var make = '';
-    var zip = "Portland, OR";
+    var zip = "97210";
     var before = date_before.getTime()/1000.0;
     var after = date_after.getTime()/1000.0;
+    var newbike = new Bike(color, make, zip, before, after);
 
-    var bikes = newbike.getBike(color, make, zip, before, after);
+    var bikes = newbike.getBike(displayBikes, displayZips);
       // console.log(bikes);
-      setTimeout(bikeText, 1500);
-
-      function bikeText(){
-        $(".showbikes").text(newbike.abikes);
-      }
+      // setTimeout(bikeText, 5000);
+      //
+      // function bikeText(){
+      //   $(".showbikes").text(newbike.abikes);
+      // }
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -1,21 +1,41 @@
-function Bike(){
+function Bike(color, make, zip, before, after){
+  this.color = color;
+  this.make = make;
+  this.zip = zip;
+  this.before = before;
+  this.after = after;
   this.abikes = 0;
 }
 
 
-Bike.prototype.getBike = function(color, make, zip, before, after) {
+Bike.prototype.getBike = function(displayBikes, displayZips) {
   var that = this;
-  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=100&colors=' + color + '&manufacturer=' + make + '&proximity=' + zip + '&proximity_square=5&stolen_before='+ before + '&stolen_after=' + after).then(function(response) {
+  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=100&colors=' + that.color + '&manufacturer=' + that.make + '&proximity=' + that.zip + '&proximity_square=5&stolen_before='+ that.before + '&stolen_after=' + that.after).then(function(response) {
       // for (var bike of response.bikes) {
       //   var title = bike.title;
       //   bikes.push(bike);
       // }
-      // console.log(that);
-      that.abikes = response.bikes.length;
-      // console.log(that);
+      // console.log(that.zip);
+      that.getZips(response.bikes, displayZips);
+      displayBikes(response.bikes.length);
     });
     // console.log("abikes after request: " + this.abikes);
     // return this.abikes;
+};
+
+Bike.prototype.getZips = function(bikes, displayZips) {
+  var ziparray = [];
+  for (var bike of bikes) {
+    var location = bike.stolen_location;
+    var part = location.split(",");
+    var stolenzip = part[2];
+    if(ziparray.indexOf(stolenzip) === -1 && !isNaN(stolenzip)) {
+      ziparray.push(stolenzip);
+      console.log(ziparray);
+    }
+  }
+
+  displayZips(ziparray);
 };
 
 exports.bikeModule = Bike;
